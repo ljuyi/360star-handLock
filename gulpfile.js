8b7a5cb8ba871ff7be9368531f49ace31c699089
ljuyi <981765731@qq.com>
2017-05-11 08:55:19 +0800

    第一次提交

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+// CSS相关
+var postcss = require('gulp-postcss');
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
+var scss = require('postcss-scss');
+var stripInlineComments = require('postcss-strip-inline-comments');
+
+var browserSync = require('browser-sync'); // 自动刷新
+var plumber = require('gulp-plumber'); // gulp 错误处理
+
+var rename = require('gulp-rename'); // 文件重命名
+
+gulp.task('cleanJS', ()=>{
+    gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'))
+});
+
+gulp.task('scss', function () {
+    var postCssPlugins = [
+        stripInlineComments,
+        precss,
+        autoprefixer({
+            browsers: ['> 1%', 'IE > 8', 'Android >= 1.6', 'iOS >= 1.0']
+        })
+    ];
+
+    function buildCss() {
+        gulp.src('./src/scss/style.scss')
+            .pipe(plumber())
+            .pipe(postcss(postCssPlugins, {syntax: scss}))
+            .pipe(rename(function (path) {
+                path.extname = '.css';
+            }))
+            .pipe(cleanCSS())
+            .pipe(gulp.dest('./dist/css'));
+    }
+    buildCss();
+    gulp.watch('./src/scss/*.scss', function () {
+        buildCss();
+    })
+});
+
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        port: 2333,
+        server: {
+            baseDir: './',
+            index: 'index.html'
+        }
+    });
+
+    browserSync.watch('./dist/css/*.css').on('change', browserSync.reload);
+    browserSync.watch('./*.html').on('change', browserSync.reload)
+});
+
+gulp.task('default', ['browserSync', 'scss', 'cleanJS']);
